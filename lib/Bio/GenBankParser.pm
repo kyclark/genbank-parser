680bdbb35b97fa1232508e6a28a090d8b029f9d8
kyclark <kyclark@562e1de6-7b44-0410-8e23-a54c0e221854>
2008-06-20 19:00:37 +0000

    Upped version.

--- lib/Bio/GenBankParser.pm
+++ lib/Bio/GenBankParser.pm
@@ -19,11 +19,11 @@ Bio::GenBankParser - Parse::RecDescent parser for a GenBank record
 
 =head1 VERSION
 
-Version 0.02
+Version 0.04
 
 =cut
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 
 =head1 SYNOPSIS
 
